a452b54038fd41564d75c7e9d406cc3930065f67
Ziqi Rong <ziqirong01@gmail.com>
2023-03-13 22:07:11 -0400

    Multiprocessing

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # ENCODE-crawler
 
-A python script to crawl the ENCODE project files metadata with given targets (like epigenomic features) and cell lines. This script will try to crawl all files with any combination of the targets and the cell lines.
+A python multiprocessing script to crawl the ENCODE project files metadata with given targets (like epigenomic features) and cell lines. This script will try to crawl all files of experiments on human cells with any combination of the targets and the cell lines.
 
 ## How to use
 
 1. Enter the targets you want to query in `targets.txt`, one target per line (Existing records in this file are just examples, feel free to delete them, as well as the following `celllines.txt`).
 2. Enter the cell lines you want to query in `celllines.txt`, one cell line per line.
-3. Run the script by `python3 crawl.py`. You will see the progress bars.
+3. Run the script by `python3 crawl.py`. You will see the progress bars. (This is a multiprocessing script. We highly recommend you to use this for speeding up. If you need a single process version, you can also use `python3 crawl_singleprocess.py` instead. However, it could be very slow.)
 4. When finished, the results will be saved to `results.csv`.
 
 ## Contact
--- crawl.py
+++ crawl.py
@@ -1,6 +1,27 @@
+import csv
 import requests
-import pandas as pd
+import multiprocessing as mp
 from tqdm import tqdm
+
+fp = open('results.csv', 'w')
+writer = csv.DictWriter(fp, fieldnames=[
+    'accession',
+    'from_dataset',
+    'cell_line',
+    'target',
+    'assay_term_name',
+    'lab',
+    'date_created',
+    'file_format',
+    'file_size_MB',
+    'mapped_read_length',
+    'mapped_run_type',
+    'assembly',
+    'download_link',
+    's3_uri'
+])
+writer.writeheader()
+fp.flush()
 
 def read_inputs():
     with open('./targets.txt') as f:
@@ -21,9 +42,30 @@ def crawl_file(id):
     result = response.json()
     return result
 
+def record_file(id):
+    file = crawl_file(id)
+    record = {
+        'accession': file.get('accession', None),
+        'from_dataset': file.get('dataset', None),
+        'cell_line': file.get('biosample_ontology', None)['term_name'],
+        'target': file.get('target', None)['label'],
+        'assay_term_name': file.get('assay_term_name', None),
+        'lab': file.get('lab', None)['title'],
+        'date_created': file.get('date_created', None)[:10],
+        'file_format': file.get('file_format', None),
+        'file_size_MB': round(file.get('file_size', 0)/(1024*1024)),
+        'mapped_read_length': file.get('mapped_read_length', None),
+        'mapped_run_type': file.get('mapped_run_type', None),
+        'assembly': file.get('assembly', None),
+        'download_link': file.get('azure_uri', None),
+        's3_uri': file.get('s3_uri', None)
+    }
+    writer.writerow(record)
+    fp.flush()
+
 def main():
+    pool = mp.Pool(processes=mp.cpu_count())
     targets, cell_lines = read_inputs()
-    records = []
     try:
         for cell_line in tqdm(cell_lines, desc='cell lines', position=0):
             for target in tqdm(targets, desc='targets', position=1, leave=False):
@@ -33,34 +75,16 @@ def main():
                     for experiment in tqdm(experiments, desc='experiments', position=2, leave=False):
                         if 'files' in experiment.keys():
                             files = experiment['files']
-                            for file in tqdm(files, desc='files', position=3, leave=False):
-                                file = crawl_file(file['@id'])
-                                record = {
-                                    'accession': file.get('accession', None),
-                                    'experiment_accession': experiment.get('accession', None),
-                                    'cell_line': experiment.get('biosample_ontology', None)['term_name'],
-                                    'target': experiment.get('target', None)['label'],
-                                    'assay_term_name': experiment.get('assay_term_name', None),
-                                    'lab': experiment.get('lab', None)['title'],
-                                    'date_created': file.get('date_created', None)[:10],
-                                    'file_format': file.get('file_format', None),
-                                    'file_size_MB': round(file.get('file_size', 0)/(1024*1024)),
-                                    'mapped_read_length': file.get('mapped_read_length', None),
-                                    'mapped_run_type': file.get('mapped_run_type', None),
-                                    'assembly': file.get('assembly', None),
-                                    'download_link': file.get('azure_uri', None),
-                                    's3_uri': file.get('s3_uri', None)
-                                }
-                                records.append(record)
+                            file_ids = map(lambda x: x['@id'], files)
+                            pool.map(record_file, file_ids)
     except Exception as ex:
         print('Error with cell line / target: ', cell_line, '/', target)
         print('Error with Experiment: ', experiment.get('accession', None))
-        pd.DataFrame.from_records(records).to_csv('./results.csv')
         print('Error detected, files may not be complete.')
         raise(ex)
-
-    pd.DataFrame.from_records(records).to_csv('./results.csv')
-    print(len(records),'files found. Saved to results.csv successfully.')
+    pool.close()
+    pool.join()
+    fp.close()
     return None
 
 if __name__ == "__main__":
